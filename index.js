5c0ef0bf06c50784a2a760a9a355ff32abcb92b8
Paulyn <paulyntran@live.ca>
2019-03-06 05:16:46 +0000

    Completed step 3

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ function newTask(title,description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -26,11 +24,6 @@ const task1 = newTask('Clean Cat Litter', 'Take all the crap out of the litter b
 const task2 = newTask('Do Laundry', 'Crap');
 const tasks = [task1, task2];
 
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
